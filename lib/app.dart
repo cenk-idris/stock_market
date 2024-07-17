9ce877e5262842e64414c62c2b0107d948153797
cenk-idris <114963355+cenk-idris@users.noreply.github.com>
2024-07-17 03:40:06 +0300

    Add Firebase and Bloc dependencies

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StonksApp extends StatelessWidget {
+  const StonksApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'StonksApp',
+      home: Placeholder(),
+    );
+  }
+}
