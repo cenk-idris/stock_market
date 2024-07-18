c2cc089c8b08e17fd591de6fae389f9e44e79a69
cenk-idris <114963355+cenk-idris@users.noreply.github.com>
2024-07-18 23:55:55 +0300

    Add constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,22 @@
+final List<String> stockSymbols = [
+  'AAPL', // Apple Inc.
+  'MSFT', // Microsoft Corporation
+  'AMZN', // Amazon.com Inc.
+  'GOOGL', // Alphabet Inc. Class A
+  'FB', // Facebook, Inc.
+  'TSLA', // Tesla, Inc.
+  'BRK.B', // Berkshire Hathaway Inc. Class B
+  'JPM', // JPMorgan Chase & Co.
+  'JNJ', // Johnson & Johnson
+  'V', // Visa Inc.
+  'WMT', // Walmart Inc.
+  'PG', // Procter & Gamble Co.
+  'MA', // Mastercard Incorporated
+  'NVDA', // NVIDIA Corporation
+  'HD', // The Home Depot, Inc.
+  'DIS', // The Walt Disney Company
+  'PYPL', // PayPal Holdings, Inc.
+  'UNH', // UnitedHealth Group Incorporated
+  'VZ', // Verizon Communications Inc.
+  'KO', // The Coca-Cola Company
+];
